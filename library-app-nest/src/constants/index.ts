aff9247bdc4901bb67b3a67fc92657ccaea41972
cat-in-magic-hat <nniki.feliz@gmail.com>
2021-08-10 20:31:21 +0300

    firebase

--- library-app-nest/src/constants/index.ts
+++ library-app-nest/src/constants/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
 
 export const BOOKS_DB_SETTINGS = {
-    connectionString: `${process.env.DB_HOST}/${process.env.DB_NAME}`
+    firebaseDbUrl: process.env.FIREBASE_DB_URL
 }
